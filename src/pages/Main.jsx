ca8185e70c3fed5d6f98e83012be4fd4aefe2826
sme12 <smetanoioioi@gmail.com>
2020-05-23 18:18:02 +0300

    🐶 introduce articles cards section for the home page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import Hero from '../components/Hero';
 import { Grid, GridItem } from '../components/styles/Grid';
+import { BaseLink } from '../components/shared/Link';
 import { BaseButton } from '../components/shared/Button';
 import { Toggle } from '../components/shared/Checkbox';
 import Suggestions from '../components/shared/Suggestions';
@@ -69,6 +71,70 @@ const HomeControls = () => {
     );
 }
 
+const ArticleCardStyles = styled.div`
+    cursor: pointer;
+    padding: 0 16px;
+
+    a {
+        display: flex;
+        flex-direction: column;
+        text-decoration: none;
+        color: var(--white);
+        height: 100%;
+    }
+
+    .icon {
+        margin: 0 auto 16px auto;
+        width: 96px;
+        height: 96px;
+    }
+
+    h2 {
+        margin-bottom: 16px;
+        font-size: 1.5rem;
+        font-weight: bold;
+        line-height: 1.5;
+        text-align: center;
+    }
+
+    p {
+        margin-bottom: 16px;
+        font-size: 1rem;
+        line-height: 1.5;
+    }
+
+    .linkWrapper {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        align-items: center;
+        justify-content: flex-end;
+    }
+`;
+
+const ArticleCard = ({ heading, text, icon }) => {
+    const [state, setHover] = useState({ hover: false });
+    return (
+    <ArticleCardStyles
+        onMouseOver={() => setHover({ hover: true })}
+        onMouseOut={() => setHover({ hover: false })} 
+    >
+        <a href="/">
+            <div className="metaWrapper">
+                <div class="icon"><img src={icon} alt={heading}/></div>
+                <h2>{heading}</h2>
+                <p>{text}</p>
+            </div>
+            <div className="linkWrapper">
+                <BaseLink 
+                    className={state.hover && 'hover'}
+                >Читать дальше</BaseLink>
+            </div>
+        </a>
+    </ArticleCardStyles>
+    )
+}
+
 const Main = () => {
     return (
         <div>
@@ -78,8 +144,8 @@ const Main = () => {
             >
                 <HomeControls />
             </Hero>
-            <ContentStyles>
-                <div style={{paddingTop: '56px', paddingBottom: '48px', fontSize: '24px'}}>
+            <section style={{paddingTop: '56px', paddingBottom: '48px', fontSize: '24px'}}>
+                <ContentStyles>
                     <p style={{margin: '0 auto 56px auto', maxWidth: '757px', textAlign: 'center'}}>
                         Или посмотрите среди этих отличных ребят, которые прямо сейчас ищут свой дом
                     </p>
@@ -99,8 +165,38 @@ const Main = () => {
                             <BaseButton>Перейти к объявлениям</BaseButton>
                         </GridItem>
                     </Grid>
-                </div>
-            </ContentStyles>
+                </ContentStyles>
+            </section>
+            <section style={{paddingTop: '40px', paddingBottom: '24px', backgroundColor: 'var(--bg-grey)'}}>
+                <ContentStyles>
+                        <Grid
+                            templateColumns="repeat(3, 1fr)"
+                            alignItems="center"
+                        >
+                            <GridItem column="auto">
+                                <ArticleCard
+                                    icon="./icon-questions.svg"
+                                    heading="У меня масса вопросов!"
+                                    text="В формате вопрос-ответ рассказываем о самом важном в адаптации собаки в семью. Рекомендации к прочтению - практически на уровне “обязательно”."
+                                ></ArticleCard>
+                            </GridItem>
+                            <GridItem column="auto">
+                                <ArticleCard
+                                    icon="./icon-list.svg"
+                                    heading="Как подготовиться к собаке?"
+                                    text="Ок, вы точно убедились, что морально готовы к собаке. Что теперь? Коротенький чек-лист для будущего хозяина собаки из приюта."
+                                ></ArticleCard>
+                            </GridItem>
+                            <GridItem column="auto">
+                                <ArticleCard
+                                    icon="./icon-care.svg"
+                                    heading="Я хочу помочь по-другому"
+                                    text="По каким-то причинам вы не можете взять собаку, но очень хотите ей помочь. Разбираемся, как работает кнопка “Стать куратором” и подходит ли вам эта опция."
+                                ></ArticleCard>
+                            </GridItem>
+                        </Grid>
+                </ContentStyles>
+            </section>
         </div>
     );
 };
